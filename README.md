20d504c454ca5baec9ab9dcb0affda6f29c4a45d
LiyanNguyen <50958126+LiyanNguyen@users.noreply.github.com>
2022-04-25 00:50:05 +0800

    Added files

---
+++ README.md
@@ -0,0 +1,44 @@
+# Tip Calculator App
+A Tip Calculator App that calculates the tip and total amount to be paid by each person according to a bill
+
+### Frontend Mentor - Tip calculator app solution
+This is a solution to the [Tip calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/tip-calculator-app-ugJNGbJUX).
+
+### Screenshot
+
+### Links
+- Solution URL: 
+- Live Site URL: 
+
+### My Process
+- Design the Desktop Viewport WHILE desinging the Mobile viewport at the same time
+- Design the flex/grid system, making sure to have both Desktop and Mobile Viewport layout correct
+- Add in all the other HTML content
+- Finished up all the initial static CSS designs
+- Add in the hover and focus CSS states
+- Add JS functions to the pre-determine tip buttons and custom tip input
+- Add JS to make calculations everytime an input is made
+- Add JS to make the reset buttons change (there is 2 reset buttons overlaying one another)
+- Add JS for the reset button
+- Add JS to prevent user from using the - and + keys
+- Final testings before deployment
+
+### Built with
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Grid
+- Desktop-first workflow
+
+### What I Learned
+- More Flexbox
+- Flex-wrap trick - to make row into columns on smaller viewports (depeding on element's width)
+- Flex and margin-top:auto trick - to make the reset button always be on bottom of the flex
+- Always use vh/vw as a starting point, then %width and %height then use rem units on smaller things
+
+## Author
+- Website - [Liyan Anajao](https://liyannguyen.github.io/Portfolio)
+- Frontend Mentor - [@LiyanNguyen](https://frontendmentor.io/profile/LiyanNguyen)
+- Twitter - [@LiyanAnajao](https://twitter.com/LiyanAnajao)
+
+### Acknowledgments
---
+++ index.js
@@ -0,0 +1,117 @@
+let billInput = document.querySelector("#bill");
+let customTipInput = document.querySelector("#customTip");
+let peopleNumberInput = document.querySelector("#people");
+let tipAmountPerPerson = document.querySelector("#tipAmountPerPerson");
+let totalPerPerson = document.querySelector("#totalPerPerson");
+let tipButtons = document.querySelectorAll(".tipChoices button");
+let errorMessage = document.querySelector(".peopleContainer p");
+let validResetButton = document.querySelector("#validReset");
+
+let bill = 0;
+let tip = 0;
+let numberOfPeople = 0;
+let TipAmount = 0;
+let TotalAmount = 0;
+
+billInput.oninput = () => {
+	if (billInput.value != "") {
+		bill = billInput.value;
+		caculateTipAndTotal();
+	}
+};
+
+customTipInput.oninput = () => {
+	if (customTipInput.value != "") {
+    tip = customTipInput.value;
+  } else {
+    tip = 0;
+  }
+	tipButtons.forEach((button) => {
+		button.classList.remove("selectedTip");
+	});
+	caculateTipAndTotal();
+};
+
+customTipInput.onfocus = () => {
+	if (customTipInput.value != "") {
+    tip = customTipInput.value;
+  } else {
+    tip = 0;
+  }
+	tipButtons.forEach((button) => {
+		button.classList.remove("selectedTip");
+	});
+	caculateTipAndTotal();
+};
+
+peopleNumberInput.oninput = () => {
+	if (peopleNumberInput.value != "" && peopleNumberInput.value != 0) {
+		numberOfPeople = peopleNumberInput.value;
+		peopleNumberInput.classList.remove("invalidInput");
+		errorMessage.style.display = "none";
+		caculateTipAndTotal();
+	}
+
+	else if (peopleNumberInput.value == "" || peopleNumberInput.value == 0) {
+		peopleNumberInput.classList.add("invalidInput");
+		errorMessage.style.display = "block"
+		tipAmountPerPerson.innerText = `$0.00`;
+		totalPerPerson.innerText = `$0.00`;
+		validResetButton.style.display = "none";
+	}
+};
+
+let caculateTipAndTotal = () => {
+	// allow to give no tip at all
+	if (bill != 0 && numberOfPeople != 0) {
+		TipAmount = (bill * (tip / 100)) / numberOfPeople;
+		TotalAmount = (bill * (1 + tip / 100)) / numberOfPeople;
+
+		// smart solution to always show 2 decimal places but not rounding on the 2nd decimal place
+		tipAmountPerPerson.innerText = `$${TipAmount.toFixed(3).slice(0, -1)}`;
+
+		//weird, this one we round to 2 decimal places - accoding to the design anyway
+		totalPerPerson.innerText = `$${TotalAmount.toFixed(2)}`;
+		
+		validResetButton.style.display = "block";
+	}
+};
+
+tipButtons.forEach((button) => {
+	button.onclick = () => {
+		tipButtons.forEach((allButtons) => {
+			allButtons.classList.remove("selectedTip");
+		});
+		button.classList.add("selectedTip");
+		tip = button.innerText.slice(0, -1);
+		customTipInput.value = "";
+		caculateTipAndTotal();
+	};
+});
+
+validResetButton.onclick = () => {
+	bill = 0;
+	tip = 0;
+	numberOfPeople = 0;
+	TipAmount = 0;
+	TotalAmount = 0;
+	billInput.value = "";
+	customTipInput.value = "";
+	peopleNumberInput.value = "";
+	tipAmountPerPerson.innerText = `$0.00`;
+	totalPerPerson.innerText = `$0.00`;
+
+	tipButtons.forEach((allButtons) => {
+		allButtons.classList.remove("selectedTip");
+	});
+
+	validResetButton.style.display = "none";
+};
+
+// dont allow user to use the - and + for the inputs
+// so we dont have to deal with negative inputs in the first place
+window.addEventListener("keydown", (event) => {
+	if (event.key == "-" || event.key == "+") {
+    event.preventDefault();
+  }
+});
